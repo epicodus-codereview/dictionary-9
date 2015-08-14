33f04fa7e9b8441c40eeb316a64bf820935cc182
timkellogg <tim.kellogg@gmail.com>
2015-08-14 13:11:59 -0700

    dry up specs by replacing local variable creation with instance variable

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,31 +3,31 @@ require 'definition'
 
 describe Definition do
 
+  before do
+    @definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
+  end
+
   describe '#text' do
     it 'should return the text of the definition' do
-      definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
-      expect(definition.text).to eq 'a unit of language'
+      expect(@definition.text).to eq 'a unit of language'
     end
   end
 
   describe '#part_of_speech' do
     it 'should return the part of speech of the definition' do
-      definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
-      expect(definition.part_of_speech).to eq 'noun'
+      expect(@definition.part_of_speech).to eq 'noun'
     end
   end
 
   describe '#example' do
     it 'should return the example of the word being used in the definition' do
-      definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
-      expect(definition.example).to eq 'The word of the day is bacon.'
+      expect(@definition.example).to eq 'The word of the day is bacon.'
     end
   end
 
   describe '#id' do
     it 'should return the id of the definition' do
-      definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
-      expect(definition.id).to eq 1
+      expect(@definition.id).to eq 1
     end
   end
 end

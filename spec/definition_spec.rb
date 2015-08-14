9e841806dc09b2403ae8f0ff971be4bfab85eb06
timkellogg <tim.kellogg@gmail.com>
2015-08-14 09:40:25 -0700

    add ability to retrieve textual definition

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,2 +1,19 @@
 require 'rspec'
 require 'definition'
+
+describe Definition do
+
+  describe '#text' do
+    it 'should return the text of the definition' do
+      definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
+      expect(definition.text).to eq 'a unit of language'
+    end
+  end
+end
+
+
+# def initialize (attributes)
+#   @text           = attributes.fetch :text
+#   @part_of_speech = nil
+#   @example        = nil
+# end

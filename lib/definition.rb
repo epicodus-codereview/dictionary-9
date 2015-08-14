2243b83e1ec24290d229b02741f145de3ed9ee1d
timkellogg <tim.kellogg@gmail.com>
2015-08-14 09:42:18 -0700

    add method to retrieve the part of speech of the definition

--- lib/definition.rb
+++ lib/definition.rb
@@ -4,8 +4,8 @@ class Definition
 
   def initialize (attributes)
     @text           = attributes.fetch :text
-    @part_of_speech = nil
-    @example        = nil
+    @part_of_speech = attributes.fetch :part_of_speech
+    @example        = attributes.fetch :example
   end
 
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -9,6 +9,13 @@ describe Definition do
       expect(definition.text).to eq 'a unit of language'
     end
   end
+
+  describe '#part_of_speech' do
+    it 'should return the part of speech of the definition' do
+      definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})
+      expect(definition.part_of_speech).to eq 'noun'
+    end
+  end
 end
 
 

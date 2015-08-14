29d83b58e679c6a958a47be7f22857bbbdd4d901
timkellogg <tim.kellogg@gmail.com>
2015-08-14 11:23:48 -0700

    update gem dependencies with launchy

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,6 +3,10 @@ require 'definition'
 
 describe Definition do
 
+  before do
+    Definition.clear 
+  end
+
   describe '#text' do
     it 'should return the text of the definition' do
       definition = Definition.new({text: 'a unit of language', part_of_speech: 'noun', example: 'The word of the day is bacon.'})

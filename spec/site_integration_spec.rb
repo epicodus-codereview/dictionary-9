f3b353405d6799c66a0920db81f8ad670fbb7dce
timkellogg <tim.kellogg@gmail.com>
2015-08-14 13:21:57 -0700

    improve integration spec readability

--- spec/site_integration_spec.rb
+++ spec/site_integration_spec.rb
@@ -6,9 +6,8 @@ require './app'
 Capybara.app = Sinatra::Application
 
 describe 'when the user views the index page', { :type => :feature } do
-  before do
-    visit '/'
-  end
+
+  before { visit '/' }
 
   it 'should display a form' do
     expect(page).to have_content 'Dictionary'
@@ -34,24 +33,14 @@ describe 'when the user views the index page', { :type => :feature } do
 
     it 'should allow the user to create a new definition for the word' do
       click_link 'Bacon'
-      fill_in 'text',           with: 'an awesome food'
+      fill_in 'text',                 with: 'an awesome food'
       fill_in 'part_of_speech',       with: 'noun'
-      fill_in 'example', with: 'The comedian Jim Gaffigan is obsessed with bacon.'
+      fill_in 'example',              with: 'The comedian Jim Gaffigan is obsessed with bacon.'
       click_button 'Add the definition!'
       expect(page).to have_content 'an awesome food'
       expect(page).to have_content 'The comedian Jim Gaffigan is obsessed with bacon.'
     end
   end
-
-  # Spec fails probably because it cannot detect the html required tag; functionality still works as expected
-  # context 'when not filling in some required elements' do
-  #   it 'should prevent the user from moving to the next page' do
-  #     visit '/'
-  #     fill_in 'Word to Define', with: ''
-  #     click_button 'Add Word'
-  #     expect(page).to have_content 'Add Word'
-  #   end
-  # end
 end
 
 
